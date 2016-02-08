da668016a937a84776c1585706e1bda5ce81ab6a
TheBerberiCode <berberi.safet@gmail.com>
2016-02-08 17:41:11 -0500

    conerted DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -72,14 +72,22 @@ app.post('/todos',function(req,res){
 
 app.delete('/todos/:id', function(req,res){
 	 var idToDelete = parseInt(req.params.id,10);
-	 var matchedTodo = _.findWhere(todos,{id: idToDelete});
-
-	 if(!matchedTodo){
-	 	res.status(404).json({"error": "no todo found with that id"});
-	 }else{
-	 	todos = _.without(todos,matchedTodo);
-	 	res.json(matchedTodo);
-	 }
+	 
+	 db.todo.destroy({
+	 	where:{
+	 		id:idToDelete
+	 	}
+	 }).then(function(rowsDeleted){
+	 	if(rowsDeleted === 0) {
+	 		res.status(404).json({
+	 			error: 'No todo with id'
+	 		});
+	 	}else {
+	 		res.status(204).send();
+	 	}
+	 }, function(){
+	 		res.status(500).send();
+	 });
 });
 
 //PUT /todos/:id
